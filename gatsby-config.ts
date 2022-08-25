4a95c973ba9c1a0cef271e894376ea09150fb3e5
some1chan <31833556+some1chan@users.noreply.github.com>
2022-08-25 03:52:07 -0500

    chore: prettier formatting

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,30 +16,30 @@ const config: GatsbyConfig = {
 		"gatsby-plugin-image",
 		"gatsby-plugin-sitemap",
 		{
-			resolve: 'gatsby-plugin-manifest',
+			resolve: "gatsby-plugin-manifest",
 			options: {
-				"icon": "src/images/icon.png"
-			}
+				icon: "src/images/icon.png",
+			},
 		},
 		"gatsby-plugin-mdx",
 		{
 			resolve: `gatsby-plugin-sharp`,
 			options: {
-			  defaults: {
-				formats: [`auto`, `webp`],
-				placeholder: `blurred`,
-				quality: 50,
-				breakpoints: [750, 1080, 1366, 1920],
-				backgroundColor: `transparent`,
-				tracedSVGOptions: {},
-				blurredOptions: {},
-				jpgOptions: {},
-				pngOptions: {},
-				webpOptions: {},
-				avifOptions: {},
-			  }
-			}
-		  },
+				defaults: {
+					formats: [`auto`, `webp`],
+					placeholder: `blurred`,
+					quality: 50,
+					breakpoints: [750, 1080, 1366, 1920],
+					backgroundColor: `transparent`,
+					tracedSVGOptions: {},
+					blurredOptions: {},
+					jpgOptions: {},
+					pngOptions: {},
+					webpOptions: {},
+					avifOptions: {},
+				},
+			},
+		},
 		"gatsby-transformer-sharp",
 		{
 			resolve: "gatsby-source-filesystem",
